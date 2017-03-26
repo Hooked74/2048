c432bdcde662c716b50b838b280a90e1e2cecceb
Новиков Игорь <novikovio74@gmail.com>
2017-03-26 21:58:31 +0500

    Added board and tiles constants

---
+++ src/app/constants/board.ts
@@ -0,0 +1,4 @@
+export const BOARD_SIDE_LENGTH:number = 4;
+export const BOARD_CELL_WIDTH:number = 107;
+export const BOARD_CELL_HEIGHT:number = 107;
+export const BOARD_CELL_GAP:number = 15;
---
+++ src/app/constants/tiles.ts
@@ -0,0 +1,3 @@
+export const TILE_FIRST_VALUE:number = 2;
+export const TILE_SECOND_VALUE:number = 4;
+export const TILE_FACTOR:number = 0.9;
